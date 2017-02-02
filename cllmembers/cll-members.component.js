30a05c377c8e8d76638fee67379773e9bf8375e1
ahmed ben hariz <benharizahmed@gmail.com>
2017-02-02 11:20:07 +0100

    init angular

---
+++ cllmembers/cll-members.component.js
@@ -0,0 +1,31 @@
+'use strict';
+angular.
+module('cllm').
+component('cllm',{
+  templateUrl: 'cllmembers/cll-members.template.html',
+  controller:[ function cllMembersController(){
+      /*this.order='age';
+      this.search="";
+     this.members=[{
+    prenom:"zaineb",
+    nom:"lamine",
+    age:23,
+    poste:"externe"
+    },
+    {
+    prenom:"ahmed",
+    nom:"ben hariz",
+    age:24,
+    poste:"materiel"
+    },
+    {
+      prenom:"achref",
+      nom: "bk",
+      age: 25,
+      poste : "membre"
+
+    }
+  ];*/
+}
+]
+});
